464b89c3ca17bfc639b033bff17a4fc997932487
Geoff Ostrega <geoff.ostrega@softledger.com>
2018-04-25 09:21:39 -0700

    added readme

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const readline = require('readline');
 const reactDocgen = require('react-docgen');
 const ReactDocGenMarkdownRenderer = require('react-docgen-markdown-renderer');
 /**
@@ -34,26 +35,41 @@ const renderer = new ReactDocGenMarkdownRenderer({
 //output path
 const readmeFile = path.join(BASE_PATH, OUTPUT);
 
-//empty file
+//set up reading template file
+const rl = readline.createInterface({
+  input: fs.createReadStream(path.join(BASE_PATH, TEMPLATE)),
+  crlfDelay: Infinity
+});
+
+
+//clean ouptut file if exists already
 fs.writeFileSync(readmeFile, '');
 
-fs.appendFileSync(readmeFile, fs.readFileSync(path.join(BASE_PATH, TEMPLATE)))
+//read the template line by line, 
+rl.on('line', line => {
+  //check if line is our special write marker
+  if(line === '<COMPONENT PROPS>') {
+    writeProps();
+  } else {
+    //just write the line
+    fs.appendFileSync(readmeFile, `${line}\n`);
+  }
+})
 
-//loop through all files in path
-fs.readdirSync(componentPath).forEach(file => {
-  //only do jsx files
-  if(file.indexOf('.jsx') === -1) return;
-  //console.log("file", file);
-  const content = fs.readFileSync(path.join(componentPath, file));
-  //read each file
-  const doc = reactDocgen.parse(content);
-
-  //append the results to the file
-  fs.appendFileSync(readmeFile, renderer.render(
-  /* The path to the component, used for linking to the file. */
-  `${componentPath}${file}`,
-  /* The actual react-docgen AST */
-  doc,
-  /* Array of component ASTs that this component composes*/
-  []))
-});
+//writes the props
+function writeProps() {
+  //loop through all files in path
+  fs.readdirSync(componentPath).forEach(file => {
+    //only do jsx files
+    if(file.indexOf('.jsx') === -1) return;
+    //append the results to the file
+    fs.appendFileSync(readmeFile, renderer.render(
+      /* The path to the component, used for linking to the file. */
+      `${componentPath}${file}`,
+      /* The actual react-docgen AST */
+      reactDocgen.parse(fs.readFileSync(path.join(componentPath, file))),
+      /* Array of component ASTs that this component composes*/
+      []
+    ))
+  });
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# insert-react-docgen-markdown
+Command line wrapper for [react-docgen-markdown-renderer](https://github.com/OriR/react-docgen-markdown-renderer)
+
+## Install
+```
+npm install --save-dev insert-react-docgen-markdown
+```
+Or, with yarn
+```
+yarn add -D insert-react-docgen-markdown
+```
+
+## Usage
+Once installed you can run from the command line, 
+```node index.js```
+
+Or, add to your package.json scripts
+```javascript
+{
+  "docs": "node insert-react-docgen-markdown/index.js"
+}
+```
